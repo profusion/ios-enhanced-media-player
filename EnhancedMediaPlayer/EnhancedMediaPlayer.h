9775a2018d04a228578c7bf84452e4ae010b8672
Natalia Caldas <caldas@work.co>
2023-06-06 02:36:20 -0300

    Initial Commit

---
+++ EnhancedMediaPlayer/EnhancedMediaPlayer.h
@@ -0,0 +1,18 @@
+//
+//  EnhancedMediaPlayer.h
+//  EnhancedMediaPlayer
+//
+//  Created by Natalia Caldas on 06/06/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EnhancedMediaPlayer.
+FOUNDATION_EXPORT double EnhancedMediaPlayerVersionNumber;
+
+//! Project version string for EnhancedMediaPlayer.
+FOUNDATION_EXPORT const unsigned char EnhancedMediaPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnhancedMediaPlayer/PublicHeader.h>
+
+
