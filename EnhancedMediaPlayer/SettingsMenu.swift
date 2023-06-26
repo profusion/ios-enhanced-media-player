1b93601898a1672dc7e1cff22724dd55542f7411
Pedro Mota <pedro.mota@profusion.mobi>
2023-06-26 16:53:44 -0300

    feat: add settings menu
    Issue #689

---
+++ EnhancedMediaPlayer/SettingsMenu.swift
@@ -0,0 +1,40 @@
+// SettingsMenu.swift
+import SwiftUI
+
+public struct SettingsMenu: View {
+    public init() { }
+
+    public var body: some View {
+        VStack(spacing: Constants.stackSpacing) {
+            renderSettingsItem()
+            renderSettingsItem()
+            renderSettingsItem()
+        }
+        .frame(maxHeight: .infinity, alignment: .topLeading)
+        .padding(Constants.stackPadding)
+    }
+
+    @ViewBuilder private func renderSettingsItem() -> some View {
+        HStack {
+            Image(systemName: Constants.itemIcon)
+            Text(Constants.itemText)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+extension SettingsMenu {
+    enum Constants {
+        static let itemIcon: String = "gearshape.fill"
+        static let itemText: String = "Settings item"
+        static let cornerRadius: CGFloat = 12
+        static let stackSpacing: CGFloat = 8
+        static let stackPadding: CGFloat = 16
+    }
+}
+
+struct SettingsMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsMenu()
+    }
+}
