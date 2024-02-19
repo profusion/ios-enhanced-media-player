70cd92ce666d3cc61750ec83b72093ff1dc59846
Karen Lima <karen.lima@profusion.mobi>
2024-02-19 09:31:35 -0300

    feat: add playback speed menu
    * add menu with speed control
    
    Part of #14

--- Sources/EnhancedMediaPlayer/SettingsMenu.swift
+++ Sources/EnhancedMediaPlayer/SettingsMenu.swift
@@ -4,7 +4,7 @@ import SwiftUI
 
 public struct SettingsMenu: View {
     @ObservedObject private var preferences: SettingsPreferences
-        
+
     public init(preferences: SettingsPreferences) {
         self.preferences = preferences
     }
@@ -12,13 +12,12 @@ public struct SettingsMenu: View {
     public var body: some View {
         VStack(spacing: Constants.stackSpacing) {
             renderSettingsItem()
-            renderSettingsItem()
-            renderSettingsItem()
+            renderPlaybackSpeedMenu()
         }
         .frame(maxHeight: .infinity, alignment: .topLeading)
         .padding(Constants.stackPadding)
     }
-    
+
     @ViewBuilder private func renderSettingsItem() -> some View {
         HStack {
             Image(systemName: Constants.itemIcon)
@@ -26,12 +25,38 @@ public struct SettingsMenu: View {
         }
         .frame(maxWidth: .infinity, alignment: .leading)
     }
+
+    @ViewBuilder private func renderPlaybackSpeedMenu() -> some View {
+        Menu(Constants.rateItemText, systemImage: Constants.rateItemIcon) {
+            ForEach(SettingsPreferences.PlaybackSpeedValues.allCases, id: \.self) { speed in
+                renderSpeedMenuOption(speed: speed)
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .foregroundColor(.primary)
+    }
+
+    @ViewBuilder func renderSpeedMenuOption(speed: SettingsPreferences.PlaybackSpeedValues) -> some View {
+        Button(action: {
+            preferences.rate = speed
+        }) {
+            HStack {
+                if preferences.rate == speed {
+                    Image(systemName: Constants.selectedItemIcon)
+                }
+                Text(String(format: "%.2fx", speed.rawValue))
+            }
+        }
+    }
 }
 
 extension SettingsMenu {
     enum Constants {
         static let itemIcon: String = "gearshape.fill"
         static let itemText: String = "Settings item"
+        static let rateItemIcon = "gauge.with.dots.needle.67percent"
+        static let rateItemText: String = "Playback speed"
+        static let selectedItemIcon = "checkmark"
         static let cornerRadius: CGFloat = 12
         static let stackSpacing: CGFloat = 8
         static let stackPadding: CGFloat = 16
